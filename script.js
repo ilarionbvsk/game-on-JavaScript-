1a1232dcdcc1a811a8e5ba64f10ff1d341cef7c3
Ilarion Ravich <115783097+ilarionbvsk@users.noreply.github.com>
2022-11-04 09:44:24 +0000

    got to bird gravity

---
+++ script.js
@@ -0,0 +1,14 @@
+var block = document.getElementById("block");
+var hole = document.getElementById("hole");
+var character = document.getElementById("character");
+
+
+hole.addEventListener('animationiteration', () => {
+  var random = -((Math.random()*300)+150);
+  hole.style.top = random + "px";
+  // counter++;
+});
+setInterval(function() {
+var characterTop = pasetInt(window.getComputedStyle(character).getPropertyValue("top"));
+character.stlye.top = (characterTop + 3) + "px";
+},10);
